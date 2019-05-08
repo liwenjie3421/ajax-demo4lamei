fd2b7d6611ce9d7864ef755f9ca824c7b595d2ee
liwenjie <liwenjie2@jk.pingan-inc.cn>
2019-05-08 22:16:41 +0800

    fix

--- app.ts
+++ app.ts
@@ -4,12 +4,12 @@ import codeRouter from './router/codeRouter';
 
 const server = express();
 const port = 8081;
-console.log(`server is running at http://127.0.0.1:${port}`);
-server.listen(port);
+console.log(`server is running at http://127.0.0.1:${port}`)
+server.listen(port)
 
-server.use(express.static('public'));
+server.use(express.static('public'))
 
-server.use(bodyParser.urlencoded({extended: true}));
-server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
 
-server.use('/code', codeRouter);
+server.use('/code', codeRouter)
